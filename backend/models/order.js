a92fc524fce5bc0c5e9f8bf30314be813b24a651
MWaqas Akhtar <bravewiki@gmail.com>
2026-01-05 15:52:53 +0500

    Delete backend directory

--- backend/models/order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('Order', new mongoose.Schema({
-  user: Object,
-  items: Array,
-  total: Number,
-  paymentType: String,
-  status: { type:String, default:'pending' },
-  createdAt: { type: Date, default: Date.now }
-}));
